aed1ba75312a2de492a51cc7aba73a062bb61e42
Drew Anello <drew.anello@gmail.com>
2022-09-01 12:32:25 -0400

    adds README.md

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const Pokemon = require('../models/pokemon.js');
+
+
+// INDEX
+app.get('/', (req, res) => {
+res.render('index.ejs', { data: Pokemon });
+});
+
+
+// SHOW
+app.get('/:id', (req, res) => {
+res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
