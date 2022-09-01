cde4f3e55e8994379bce31686a07deabf9c91672
Drew Anello <drew.anello@gmail.com>
2022-09-01 14:36:37 -0400

    server is running

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const Pokemon = require('../models/pokemon.js');
-
+const Pokemon = require('./models/pokemon.js');
+const port = 3000
 
 // INDEX
 app.get('/', (req, res) => {
@@ -14,3 +14,6 @@ app.get('/:id', (req, res) => {
 res.render('show.ejs', { data: Pokemon[req.params.id] });
 });
 
+app.listen(port, () => {
+    console.log(`listening on port`,port)
+})
